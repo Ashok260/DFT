d2a5bc49af551689d929542625a39d8caaea5543
L ASHOK KUMAR REDDY <97145240+Ashok260@users.noreply.github.com>
2025-07-10 09:01:25 +0530

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# Assumes TREE.md has already been generated
-
-START="<!-- START OF TREE -->"
-END="<!-- END OF TREE -->"
+# Read the contents of TREE.md
 TREE_CONTENT=$(<TREE.md)
 
-awk -v start="$START" -v end="$END" -v tree="$TREE_CONTENT" '
-    BEGIN { print_tree = 0 }
-    {
-        if ($0 == start) {
-            print $0
-            print_tree = 1
-            print tree
-        } else if ($0 == end) {
-            print_tree = 0
-            print $0
-        } else if (!print_tree) {
-            print $0
-        }
-    }
+# Use awk to insert the TREE_CONTENT between the placeholders in README.md
+awk -v tree="$TREE_CONTENT" '
+  /<!-- START OF TREE -->/ { print; print tree; skip=1; next }
+  /<!-- END OF TREE -->/ { skip=0 }
+  !skip
 ' README.md > README.tmp && mv README.tmp README.md
