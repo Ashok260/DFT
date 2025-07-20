9775f1c7274695b06b10384b426f118bb66e88da
L ASHOK KUMAR REDDY <97145240+Ashok260@users.noreply.github.com>
2025-07-20 10:08:35 +0530

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-# Safely read TREE.md line-by-line into the README.md
-awk '
-  BEGIN { inside_block = 0 }
-  /<!-- START OF TREE -->/ {
-    print
-    while ((getline line < "TREE.md") > 0) print line
-    inside_block = 1
-    next
-  }
-  /<!-- END OF TREE -->/ {
-    inside_block = 0
-    print
-    next
-  }
-  !inside_block
-' README.md > README.tmp && mv README.tmp README.md
-=======
 # Read contents of TREE.md
 TREE_CONTENT=$(<TREE.md)
 
@@ -30,4 +12,3 @@ sed -e '/<!-- START OF TREE -->/,/<!-- END OF TREE -->/c\
 
 # Replace the original README with the updated one
 mv README.tmp README.md
->>>>>>> refs/remotes/origin/main
